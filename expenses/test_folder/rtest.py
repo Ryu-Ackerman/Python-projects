2af27808a30b28d6df44ec6271469ed0817ccbe1
Rimu <feruznasriddinov7@gmail.com>
2026-03-03 20:31:18 +0500

    gitignore added

--- expenses/test_folder/rtest.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import pytz
-
-timezone = pytz.timezone('Asia/Samarkand')
-date_s = datetime.now(timezone)
-year = date_s.year
-month = date_s.month
-day = date_s.day
-print(date_s.year)
